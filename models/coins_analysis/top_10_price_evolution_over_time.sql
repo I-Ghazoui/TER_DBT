d5ae9e5a47208b9688df569f4e44b0644ebe687b
i.ghazoui <i-ghazoui@live.fr>
2025-02-19 07:54:51 +0100

    modified the top_10_price_evolution_over_time file.

---
+++ models/coins_analysis/top_10_price_evolution_over_time.sql
@@ -0,0 +1,25 @@
+WITH latest_top_10_cryptos AS (
+    SELECT 
+        SYMBOL
+    FROM {{ ref('transformed_coingecko_data_v') }}
+    WHERE CREATION_DATE = (
+        SELECT MAX(CREATION_DATE) 
+        FROM {{ ref('transformed_coingecko_data_v') }}
+    )
+    ORDER BY MARKET_CAP_RANK
+    LIMIT 10
+),
+
+price_evolution_top_10 AS (
+    SELECT 
+        SYMBOL,
+        NAME,
+        CURRENT_PRICE,
+        CREATION_DATE
+    FROM {{ ref('transformed_coingecko_data_v') }}
+    WHERE SYMBOL IN (SELECT SYMBOL FROM latest_top_10_cryptos)
+)
+
+SELECT *
+FROM price_evolution_top_10
+ORDER BY SYMBOL, CREATION_DATE
