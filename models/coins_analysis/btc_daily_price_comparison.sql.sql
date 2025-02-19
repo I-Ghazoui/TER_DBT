c385c60064c5c3a087dddc28e177d18db9208066
i.ghazoui <i-ghazoui@live.fr>
2025-02-19 08:48:54 +0100

    added btc daily price change analysis.

---
+++ models/coins_analysis/btc_daily_price_comparison.sql.sql
@@ -0,0 +1,25 @@
+
+with daily_prices as (
+    select 
+        symbol,
+        name,
+        avg(CURRENT_PRICE) as avg_daily_price,
+        date_trunc('day', created_at) as price_date
+    from {{ ref('transformed_coingecko_data_v') }}
+    where symbol = 'btc'
+        and name = 'Bitcoin'
+    group by 
+        symbol,
+        name,
+        price_date
+    order by price_date desc
+    limit 2
+)
+
+select
+    symbol,
+    name,
+    avg_daily_price as current_daily_price,
+    lead(avg_daily_price) over (order by price_date desc) as previous_daily_price
+from daily_prices
+limit 1
