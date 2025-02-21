82d2a4e3099335ab332598219a71350b06ec480d
Lahoucine Hfa <hfa.lahoucine@gmail.com>
2025-02-21 12:21:31 +0100

    Analyse selon lATH

--- models/coins_analysis/top_10_price_evolution_over_time.sql
+++ models/coins_analysis/top_10_price_evolution_over_time.sql
@@ -17,11 +17,23 @@ price_evolution_top_10 AS (
         SYMBOL,
         NAME,
         CURRENT_PRICE,
+        ATH,
         CREATION_DATE
     FROM {{ ref('transformed_coingecko_data_v') }}
     WHERE (SYMBOL, NAME) IN (SELECT SYMBOL, NAME FROM latest_top_10_cryptos)
 )
 
+distance_to_ath AS (
+    SELECT 
+        SYMBOL,
+        NAME,
+        CURRENT_PRICE,
+        ATH,
+        CREATION_DATE,
+        ((ATH - CURRENT_PRICE) / NULLIF(ATH,0)) * 100 AS DISTANCE_TO_ATH_PERCENTAGE
+    FROM price_evolution_top_10
+)
+
 SELECT *
-FROM price_evolution_top_10
+FROM distance_to_ath
 ORDER BY SYMBOL, CREATION_DATE
