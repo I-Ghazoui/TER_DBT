2563db33b913c44af4d0b5b1d6d404311e3751ff
Lahoucine Hfa <hfa.lahoucine@gmail.com>
2025-02-21 16:15:56 +0100

    supply ratio analysis

---
+++ models/coins_analysis/supply_ratio_analysis.sql
@@ -0,0 +1,31 @@
+WITH supply_analysis AS (
+    SELECT 
+        SYMBOL,
+        NAME,
+        CIRCULATING_SUPPLY,
+        TOTAL_SUPPLY,
+        MAX_SUPPLY,
+        (CIRCULATING_SUPPLY / NULLIF(TOTAL_SUPPLY, 0)) * 100 AS SUPPLY_RATIO_PERCENTAGE
+    FROM {{ ref('transformed_coingecko_data_v') }}
+    WHERE 1=1
+    AND ID IS NOT NULL
+    AND ID != ' '
+    AND NAME IS NOT NULL
+    AND NAME != ' '
+    AND SYMBOL IS NOT NULL
+    AND SYMBOL != ' '
+    AND SYMBOL NOT IN ('usdt', 'usdc', 'usds') -- Exclude stablecoins
+    LIMIT 10
+)
+
+SELECT 
+    SYMBOL,
+    NAME,
+    CIRCULATING_SUPPLY,
+    TOTAL_SUPPLY,
+    MAX_SUPPLY,
+    SUPPLY_RATIO_PERCENTAGE
+FROM supply_analysis
+ORDER BY SUPPLY_RATIO_PERCENTAGE DESC
+
+-- bar chat, we can visulaize also see the "rare" ones that can be affected by its inflation.
