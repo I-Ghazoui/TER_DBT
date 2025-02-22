805bec31f73d4c067fea07abcb1932d4ae4d7674
Lahoucine Hfa <hfa.lahoucine@gmail.com>
2025-02-23 00:27:54 +0100

    filter

--- models/coins_analysis/volatility_analysis.sql
+++ models/coins_analysis/volatility_analysis.sql
@@ -9,7 +9,7 @@ WITH base AS (
         LOW_24H,
         ATH,
         ATL,
-        CREATION_DATE
+        CREATION_DATE::DATE AS TRADE_DATE
     FROM {{ ref('transformed_coingecko_data_v') }}
     WHERE 1=1
     {% if is_incremental() %}
@@ -21,29 +21,32 @@ WITH base AS (
     AND NAME != ' '
     AND SYMBOL IS NOT NULL
     AND SYMBOL != ' '
-    AND SYMBOL NOT IN ('usdt', 'usdc', 'usds', 'wbtc', 'steth') -- Exclustion de  USDT et USDC
+    AND SYMBOL NOT IN ('usdt', 'usdc', 'usds', 'wbtc', 'steth')
+    AND ATL > 0.0001 -- Exclure les cryptomonnaies avec un ATL extrêmement bas
 ),
-
 volatility_analysis AS (
     SELECT 
         SYMBOL,
         NAME,
-        CREATION_DATE,
+        TRADE_DATE,
         (HIGH_24H - LOW_24H) / NULLIF(CURRENT_PRICE, 0) * 100 AS RELATIVE_VOLATILITY_PERCENTAGE,
         (CURRENT_PRICE - ATH) / NULLIF(ATH, 0) * 100 AS DISTANCE_TO_ATH_PERCENTAGE,
-        (CURRENT_PRICE - ATL) / NULLIF(ATL, 0) * 100 AS DISTANCE_TO_ATL_PERCENTAGE
+        (CURRENT_PRICE - ATL) / NULLIF(ATL, 0) * 100 AS DISTANCE_TO_ATL_PERCENTAGE,
+        CASE 
+            WHEN (CURRENT_PRICE - ATH) / NULLIF(ATH, 0) * 100 > -20 THEN 'Proche ATH'
+            WHEN (CURRENT_PRICE - ATH) / NULLIF(ATH, 0) * 100 BETWEEN -50 AND -20 THEN 'Moyennement Proche ATH'
+            ELSE 'Loin ATH'
+        END AS ATH_PROXIMITY_CATEGORY
     FROM base
 )
-
 SELECT 
     SYMBOL,
     NAME,
+    TRADE_DATE,
     AVG(RELATIVE_VOLATILITY_PERCENTAGE) AS AVG_RELATIVE_VOLATILITY,
     AVG(DISTANCE_TO_ATH_PERCENTAGE) AS AVG_DISTANCE_TO_ATH,
-    AVG(DISTANCE_TO_ATL_PERCENTAGE) AS AVG_DISTANCE_TO_ATL
+    AVG(DISTANCE_TO_ATL_PERCENTAGE) AS AVG_DISTANCE_TO_ATL,
+    MAX(ATH_PROXIMITY_CATEGORY) AS ATH_PROXIMITY_CATEGORY
 FROM volatility_analysis
-
-GROUP BY SYMBOL, NAME
-ORDER BY AVG_RELATIVE_VOLATILITY DESC
-
--- We can use Scatter plot for this one.
+GROUP BY SYMBOL, NAME, TRADE_DATE
+ORDER BY TRADE_DATE DESC, AVG_RELATIVE_VOLATILITY DESC
