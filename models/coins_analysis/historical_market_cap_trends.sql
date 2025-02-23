13db956f1f35b7f38bdeb73a41a1c1c16bd74464
Lahoucine Hfa <hfa.lahoucine@gmail.com>
2025-02-23 02:36:11 +0100

    historical & volatility HCN

--- models/coins_analysis/historical_market_cap_trends.sql
+++ models/coins_analysis/historical_market_cap_trends.sql
@@ -1,11 +1,11 @@
 WITH latest_date AS (
-    SELECT MAX(CREATION_DATE) AS LATEST_CREATION_DATE
+    SELECT MAX(CREATION_DATE::DATE) AS LATEST_CREATION_DATE
     FROM {{ ref('transformed_coingecko_data_v') }}
 ),
 filtered_data AS (
     SELECT *
     FROM {{ ref('transformed_coingecko_data_v') }}
-    WHERE CREATION_DATE = (SELECT LATEST_CREATION_DATE FROM latest_date)
+    WHERE CREATION_DATE::DATE = (SELECT LATEST_CREATION_DATE FROM latest_date)
 )
 SELECT 
     ID AS CRYPTO_ID,
