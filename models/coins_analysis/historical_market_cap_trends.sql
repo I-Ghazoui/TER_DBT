ac20ed4e76bc24d30c87164f4452e834aee00014
Lahoucine Hfa <hfa.lahoucine@gmail.com>
2025-02-23 04:30:40 +0100

    historical market cap

--- models/coins_analysis/historical_market_cap_trends.sql
+++ models/coins_analysis/historical_market_cap_trends.sql
@@ -1,7 +1,8 @@
 {{
     config(
         materialized='incremental',
-        unique_key=['symbol', 'date'] 
+        unique_key=["symbol", "date"], 
+        merge_update_columns=["market_cap"]
     )
 }}
 
@@ -13,14 +14,28 @@
     {% set max_date = run_query(max_date_query).columns[0][0] %}
 {% endif %}
 
+WITH deduplicated_data AS (
+    SELECT
+        CAST(creation_date AS DATE) AS date,
+        symbol,
+        name,
+        market_cap,
+        ROW_NUMBER() OVER (
+            PARTITION BY symbol, CAST(creation_date AS DATE) 
+            ORDER BY creation_date DESC
+        ) AS rn
+    FROM {{ ref('transformed_coingecko_data_v') }}
+    WHERE symbol IN ('btc', 'eth', 'usdt', 'sol', 'xrp', 'doge', 'trx', 'ada', 'shib')
+    {% if is_incremental() %}
+        AND creation_date > '{{ max_date }}'
+    {% endif %}
+)
+
 SELECT
-    CAST(creation_date AS DATE) AS date,
+    date,
     symbol,
     name,
     market_cap
-FROM {{ ref('transformed_coingecko_data_v') }}
-WHERE symbol IN ('btc', 'eth', 'usdt', 'sol', 'xrp', 'doge', 'trx', 'ada', 'shib')
-{% if is_incremental() %}
-    AND creation_date > '{{ max_date }}'
-{% endif %}
+FROM deduplicated_data
+WHERE rn = 1  -- ◀ Garde seulement la dernière entrée par (symbol, date)
 ORDER BY date ASC
