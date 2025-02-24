519cdbc6ac4d359825b83f65f19dc4da8ed39315
youcefkrookss <youcefkrooks2580@gmail.com>
2025-02-24 06:32:50 +0100

    crypto prices for nfts V4

---
+++ models/marts/nfts_trends.sql
@@ -0,0 +1,38 @@
+WITH sales_data AS (
+    SELECT
+        nft_collection,
+        COALESCE(NFT_NAME, nft_collection) AS NFT_NAME,
+        NFT_IMAGE_URL,
+        SALE_PRICE AS price,
+        NFT_TOTAL_SALES,
+        event_timestamp,
+
+        MIN(sale_price) OVER (PARTITION BY nft_collection) AS floor_price,
+
+        COUNT(DISTINCT nft_identifier) OVER (PARTITION BY nft_collection) AS supply,
+
+        SUM(sale_price) OVER (
+            PARTITION BY nft_collection
+            ORDER BY event_timestamp
+            RANGE BETWEEN INTERVAL '1 DAY' PRECEDING AND CURRENT ROW
+        ) AS "1d_vol",
+
+        (sale_price - LAG(sale_price, 1) OVER (PARTITION BY nft_collection ORDER BY event_timestamp)) AS "1d_changes",
+
+        SUM(sale_price) OVER (
+            PARTITION BY nft_collection
+            ORDER BY event_timestamp
+            RANGE BETWEEN INTERVAL '7 DAY' PRECEDING AND CURRENT ROW
+        ) AS "7d_vol",
+
+        (sale_price - LAG(sale_price, 7) OVER (PARTITION BY nft_collection ORDER BY event_timestamp)) AS "7d_changes"
+
+    FROM {{ ref('fact_sales') }}
+    WHERE event_timestamp >= DATEADD(DAY, -7, CURRENT_DATE)
+)
+
+SELECT *
+FROM sales_data
+QUALIFY RANK() OVER (PARTITION BY nft_collection ORDER BY price DESC) = 1
+ORDER BY price DESC
+LIMIT 10;
