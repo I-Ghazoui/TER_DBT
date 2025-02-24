229e4bb209f471858c93c0d7dcb6df4f78449c79
youcefkrookss <youcefkrooks2580@gmail.com>
2025-02-24 13:06:44 +0100

    crypto prices for nfts V4

--- models/marts/nfts_trends.sql
+++ models/marts/nfts_trends.sql
@@ -36,6 +36,6 @@ WITH sales_data AS (
 SELECT *
 FROM sales_data
 WHERE row_num = 1
-GROUP BY nft_collection,NFT_NAME,NFT_IMAGE_URL,price,NFT_TOTAL_SALES,event_timestamp,floor_price,supply,"1d_vol","1d_changes","7d_vol","7d_changes"
+GROUP BY nft_collection,NFT_NAME,NFT_IMAGE_URL,price,NFT_TOTAL_SALES,event_timestamp,floor_price,supply,"1d_vol","1d_changes","7d_vol","7d_changes",row_num
 ORDER BY price DESC
 LIMIT 10
