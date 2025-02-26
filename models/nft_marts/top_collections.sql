d84876e102f66ff0b5c03642a3fca4f9455994b0
i.ghazoui <i-ghazoui@live.fr>
2025-02-26 05:19:10 +0100

    modif on top_collections table.

--- models/nft_marts/top_collections.sql
+++ models/nft_marts/top_collections.sql
@@ -5,15 +5,19 @@
 WITH collection_sales AS (
     SELECT 
         collection_name,
+        nft_name,
         SUM(price) AS total_sales,
         COUNT(*) AS total_sales_count
     FROM {{ ref('stg_nft_events') }}
     WHERE collection_name IS NOT NULL
-    GROUP BY collection_name
+        AND nft_name IS NOT NULL
+        AND price IS NOT NULL
+    GROUP BY collection_name, nft_name
 )
 
 SELECT 
     collection_name,
+    nft_name,
     total_sales,
     total_sales_count
 FROM collection_sales
